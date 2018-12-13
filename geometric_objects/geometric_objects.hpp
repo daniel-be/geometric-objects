3fe857f9952690ddb99c0d8fe0b3303720718eac
Daniel Bencic <daniel.bencic@gmail.com>
2018-12-13 19:50:32 +0100

    cleaned up header only solution

--- geometric_objects/geometric_objects.hpp
+++ geometric_objects/geometric_objects.hpp
@@ -7,13 +7,27 @@ struct Point
 	double x[DIM];
 
 	Point(const Point& p);
-	Point(double x0 = 0.0, double x1 = 0.0, double x2 = 0.0);
-	Point& operator=(const Point& p);
-	bool operator==(const Point& p);
-};
 
-template<int DIM>
-double distance(const Point<DIM>& p, const Point<DIM>& q);
+	Point(double x0 = 0.0, double x1 = 0.0, double x2 = 0.0)
+	{
+		this->x[0] = x0;
+		if (DIM > 1) this->x[1] = x1;
+		if (DIM > 2) this->x[2] = x2;
+		if (DIM > 3) throw("Constructor by coordinates not implemented for DIM > 3");
+	}
+
+	Point& operator=(const Point& p)
+	{
+		for (int i = 0; i < DIM; i++) this->x[i] = p.x[i];
+		return *this;
+	}
+
+	bool operator==(const Point& p)
+	{
+		for (int i = 0; i < DIM; i++) if (this->x[i] != p.x[i]) return false;
+		return true;
+	}
+};
 
 template<int DIM>
 struct Line_segment
@@ -25,30 +39,6 @@ template <int DIM>
 Point<DIM>::Point(const Point& p)
 {
 	for (int i = 0; i < DIM; i++) this->x[i] = p.x[i];
-}
-
-template <int DIM>
-Point<DIM>::Point(double x0, double x1, double x2)
-{
-	this->x[0] = x0;
-	if (DIM > 1) this->x[1] = x1;
-	if (DIM > 2) this->x[2] = x2;
-	if (DIM > 3) throw("Constructor by coordinates not implemented for DIM > 3");
-}
-
-
-template <int DIM>
-Point<DIM>& ::Point<DIM>::operator=(const Point& p)
-{
-	for (int i = 0; i < DIM; i++) this->x[i] = p.x[i];
-	return *this;
-}
-
-template <int DIM>
-bool Point<DIM>::operator==(const Point& p)
-{
-	for (int i = 0; i < DIM; i++) if (this->x[i] != p.x[i]) return false;
-	return true;
 }
 
 template<int DIM>
